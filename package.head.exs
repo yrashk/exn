d742f2e08cb6a15de67b3af81afca8b53ba20f65
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-08 23:25:31 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(name: "exn", description: "Elixir Notation",
+                 version: :head, keywords: ["elixir", "notation", "data", "structure",
+                                            "encode", "decode", "safe"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/exn"]])
+    
