78235a62913ccbf5f34a07372fff16c6731d6d7e
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-13 08:31:46 -0700

    Add Devin Torres as a contributor to the expm package

--- package.head.exs
+++ package.head.exs
@@ -1,6 +1,8 @@
 Expm.Package.new(name: "exn", description: "Elixir Notation",
                  version: :head, keywords: ["elixir", "notation", "data", "structure",
                                             "encode", "decode", "safe"], 
+                 contributors: [[name: "Devin Torres",
+                                 email: "devin@devintorr.es"]],
                  maintainers: [[name: "Yurii Rashkovskii", 
                                 email: "yrashk@gmail.com"]],
                  repositories: [[github: "yrashk/exn"]])
